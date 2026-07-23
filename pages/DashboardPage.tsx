81bd72ccd0c4edc421f2084ae5ec43cd8cc50f32
劉雅萍 <liuyaping@liuyapingdeMacBook-Pro.local>
2026-07-23 19:47:06 +0800

    Add space before dash in Dazhi string

--- pages/DashboardPage.tsx
+++ pages/DashboardPage.tsx
@@ -6818,7 +6818,7 @@ BMD :{{bmd}}
     }
     
     const dazhiHeaderNamePart = dazhiNamesStr ? `：${dazhiNamesStr}` : "";
-    const dazhiDash = "- ";
+    const dazhiDash = " - ";
 
     out.push("");
     
